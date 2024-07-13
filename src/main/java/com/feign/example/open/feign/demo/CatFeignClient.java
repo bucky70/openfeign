1a19186f9ac08b1032ae4857a48ba9aa1ea54b33
Saikumar <41095585+bucky70@users.noreply.github.com>
2024-07-13 14:20:17 -0400

    Update CatFeignClient.java

--- src/main/java/com/feign/example/open/feign/demo/CatFeignClient.java
+++ src/main/java/com/feign/example/open/feign/demo/CatFeignClient.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name="catClient", url="https://http.cat/")
-public interface CatFeignClient {
+@FeignClient(name="imageClient", url="https://api.unsplash.com/search/photos/{id}")
+public interface ImageFeignClient {
     @GetMapping(value = "{id}", produces = MediaType.IMAGE_JPEG_VALUE)
     byte[] getImage(@PathVariable Integer id);
 }
